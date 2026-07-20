657dbe8fb46538b233837227cb36b9b954f2f5ff
agung1778 <157602405+agung1778@users.noreply.github.com>
2026-07-20 09:55:03 +0700

    env

--- resources/views/components/sections/foundation.blade.php
+++ resources/views/components/sections/foundation.blade.php
@@ -1,3 +1,7 @@
+@props([
+    'leader' => null
+])
+
 @if($leader)
 
 <section class="py-24">
@@ -9,41 +13,33 @@
             <div class="text-center">
 
                 <img
-
                     src="{{ Storage::url($leader->foundation) }}"
-
                     class="w-80 h-80 rounded-full object-cover mx-auto shadow-xl"
-
                     alt="{{ $leader->name }}"
-
                 >
 
             </div>
 
+
             <div>
 
                 <span class="text-blue-900 font-semibold">
-
                     Kepala Yayasan
-
                 </span>
 
+
                 <h2 class="mt-3 text-4xl font-bold">
-
                     {{ $leader->name }}
-
                 </h2>
 
+
                 <p class="text-yellow-600 mt-2">
-
                     {{ $leader->position }}
-
                 </p>
 
+
                 <div class="mt-8 text-slate-600 leading-8">
-
                     {{ $leader->message }}
-
                 </div>
 
             </div>
